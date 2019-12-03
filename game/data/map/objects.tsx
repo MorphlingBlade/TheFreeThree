53cf0bc02cacab1eed25d6b6bc62852656b7465a
Mission Bit <missionbit@44a-MissionBit.local>
2019-12-02 17:51:38 -0800

    add image files

---
+++ game/data/map/objects.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="objects" tilewidth="511" tileheight="280" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="101" height="101" source="../img/map/winter-objects/Crate.png"/>
+ </tile>
+ <tile id="1">
+  <image width="97" height="78" source="../img/map/winter-objects/Crystal.png"/>
+ </tile>
+ <tile id="2">
+  <image width="101" height="101" source="../img/map/winter-objects/IceBox.png"/>
+ </tile>
+ <tile id="3">
+  <image width="511" height="201" source="../img/map/winter-objects/Igloo.png"/>
+ </tile>
+ <tile id="4">
+  <image width="87" height="94" source="../img/map/winter-objects/Sign_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="87" height="93" source="../img/map/winter-objects/Sign_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="193" height="210" source="../img/map/winter-objects/SnowMan.png"/>
+ </tile>
+ <tile id="7">
+  <image width="124" height="78" source="../img/map/winter-objects/Stone.png"/>
+ </tile>
+ <tile id="8">
+  <image width="364" height="280" source="../img/map/winter-objects/Tree_1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="228" height="280" source="../img/map/winter-objects/Tree_2.png"/>
+ </tile>
+</tileset>
---
+++ game/data/map/snowy.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="snowy" tilewidth="128" tileheight="128" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../img/map/winter-tiles/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../img/map/winter-tiles/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../img/map/winter-tiles/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../img/map/winter-tiles/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../img/map/winter-tiles/5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../img/map/winter-tiles/6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../img/map/winter-tiles/7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../img/map/winter-tiles/8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../img/map/winter-tiles/9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../img/map/winter-tiles/10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../img/map/winter-tiles/11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../img/map/winter-tiles/12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../img/map/winter-tiles/13.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="93" source="../img/map/winter-tiles/14.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="93" source="../img/map/winter-tiles/15.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="93" source="../img/map/winter-tiles/16.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="99" source="../img/map/winter-tiles/17.png"/>
+ </tile>
+ <tile id="17">
+  <image width="128" height="128" source="../img/map/winter-tiles/18.png"/>
+ </tile>
+</tileset>
